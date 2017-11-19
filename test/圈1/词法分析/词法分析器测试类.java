96ab9bc587942c395e399af6dd143a35f55f4413
fromwheretowhere <fromwheretowhere.service@gmail.com>
2017-11-19 15:24:19 -0800

    条件语句-词法分析测试

--- test/圈1/词法分析/词法分析器测试类.java
+++ test/圈1/词法分析/词法分析器测试类.java
@@ -50,5 +50,21 @@ public class 词法分析器测试类 {
     assertEquals("去年年龄", token列表.get(2).取文本());
     assertEquals("+", token列表.get(3).取文本());
     assertEquals(1, token列表.get(4).取值());
+
+    token列表 = 词法分析器类.取Token列表("如果(年龄>1){\n1}\n否则{2}");
+    assertEquals(13, token列表.size());
+    assertEquals("如果", token列表.get(0).取文本());
+    assertEquals("(", token列表.get(1).取文本());
+    assertEquals("年龄", token列表.get(2).取文本());
+    assertEquals(">", token列表.get(3).取文本());
+    assertEquals(1, token列表.get(4).取值());
+    assertEquals(")", token列表.get(5).取文本());
+    assertEquals("{", token列表.get(6).取文本());
+    assertEquals("1", token列表.get(7).取文本());
+    assertEquals("}", token列表.get(8).取文本());
+    assertEquals("否则", token列表.get(9).取文本());
+    assertEquals("{", token列表.get(10).取文本());
+    assertEquals("2", token列表.get(11).取文本());
+    assertEquals("}", token列表.get(12).取文本());
   }
 }
