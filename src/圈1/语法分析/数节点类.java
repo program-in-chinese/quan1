45cda35592d46cf80cf1ca36fcb78cc0a4e3f27a
fromwheretowhere <fromwheretowhere.service@gmail.com>
2017-11-20 00:47:28 -0800

    支持多表达式

--- src/圈1/语法分析/数节点类.java
+++ src/圈1/语法分析/数节点类.java
@@ -7,7 +7,9 @@ public class 数节点类 extends 基本类型类 {
 
   static boolean 匹配(词法分析器类 词法分析器) {
     词类 词 = 词法分析器.一瞧();
-    return 词.是数();
+    
+    // 如果结束, 返回null
+    return 词 != null && 词.是数();
   }
 
   public 数节点类(词类 词) {
